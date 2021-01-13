52388de5a1e0501644cbae14dff7fc09fc20171b
Damian Borowiecki <dborowiecki@soldevelo.com>
2021-01-13 13:20:07 +0100

    OAI-29: Different types of payload in claim consumer

--- claim_ai/consumers/claim_consumer.py
+++ claim_ai/consumers/claim_consumer.py
@@ -26,13 +26,22 @@ class ClaimConsumer(AsyncConsumer):
         })
 
     async def websocket_receive(self, event):
-        index = self._assign_event_index(event)
-        await self._bundle_evaluation(event, index)
+        payload = self._get_content(event)
+        if payload['type'] == 'claim.bundle.payload':
+            index = self._assign_event_index(payload)
+            await self._bundle_evaluation(payload['content'], index)
+        elif payload['type'] == 'claim.bundle.acceptance':
+            # TODO: confirm evaluation receive and remove it from query
+            print("Payload received")
+            pass
 
     def _get_content(self, event):
         bytes_data = event.get('bytes', None)
         if bytes_data:
-            bytes_data = zlib.decompress(bytes_data)
+            try:
+                bytes_data = zlib.decompress(bytes_data)
+            except Exception as e:
+                pass
             bundle = json.loads(bytes_data.decode("utf-8"))
             return bundle
 
@@ -41,11 +50,9 @@ class ClaimConsumer(AsyncConsumer):
         self.bundle_query[event_index] = event
         return event_index
 
-    async def _bundle_evaluation(self, event, event_index):
-        content = self._get_content(event)
+    async def _bundle_evaluation(self, content, event_index):
         await self._send_acceptance(event_index)
         await self._send_evaluation(content, event_index)
-
 
     async def _send_acceptance(self, event_index):
         accept_response = { 'type': 'claim.bundle.acceptance', 'content': 'Accepted', 'index': event_index}
