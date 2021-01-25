6a280090ec0166aae245b9fb879511ed393601e0
Damian Borowiecki <dborowiecki@soldevelo.com>
2021-01-25 18:26:13 +0100

    OAI-32: Added mapping from claim response to claim output

--- claim_ai/evaluation/converter.py
+++ claim_ai/evaluation/converter.py
@@ -1,5 +1,9 @@
-from claim_ai.evaluation.converters import AiConverter
+from itertools import groupby
+from typing import List
 
+from claim_ai.evaluation.converters import AiConverter
+from claim_ai.evaluation.evaluation_result import EvaluationResult
+from claim_ai.apps import ClaimAiConfig
 
 class FHIRConverter:
     converter = AiConverter()
@@ -14,3 +18,19 @@ class FHIRConverter:
     def claim_ai_input(self, fhir_claim_repr):
         input_models = self.converter.to_ai_input(fhir_claim_repr)
         return [model.to_representation() for model in input_models]
+
+    def bundle_ai_output(self, evaluation_output: List[EvaluationResult]):
+        response_bundle = {
+            'resourceType': 'Bundle',
+            'entry': []
+        }
+
+        for claim, output in groupby(evaluation_output, lambda x: x.claim):
+            claim_fhir_response = self.converter.to_ai_output(claim, list(output))
+            entry = {
+                'fullUrl': ClaimAiConfig.claim_response_url+'/'+claim['id'],
+                'resource': claim_fhir_response
+            }
+            response_bundle['entry'].append(entry)
+
+        return response_bundle
