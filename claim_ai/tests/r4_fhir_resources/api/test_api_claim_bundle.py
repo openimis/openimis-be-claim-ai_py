7f2906dfb8be497e821402b7a64115f09de73f49
Damian Borowiecki <dborowiecki@soldevelo.com>
2022-02-17 15:35:18 +0100

    OAI-60: Test json payload directory

--- claim_ai/tests/r4_fhir_resources/api/test_api_claim_bundle.py
+++ claim_ai/tests/r4_fhir_resources/api/test_api_claim_bundle.py
@@ -150,7 +150,8 @@ class ClaimBundleAPITests(ClaimAPIContainedTestBaseMixin, GenericFhirAPITestMixi
         return json.loads(self._adjust_expected_response_string(payload))
 
     def __read_file_from_path(self, filename):
-        dir_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        print(os.listdir(dir_path), os.listdir(dir_path+'/api/'))
         return open(dir_path + filename).read()
 
     def _assert_contained(self):
